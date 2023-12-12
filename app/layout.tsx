d3373cf35f5eb4d4fa84564460428e9db0ec32ed
Diego Andres Galvis Villamizar <diego.galvis@wiedii.co>
2023-12-12 09:23:19 -0500

    Added Auth, Metadata, Handle errors and middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css'
 import { inter }  from '@/app/ui/fonts'
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: 'Dashboard | Acme dashboard',
+  description: 'The oficial Next.js course dashboard, build with app router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
+
 
 export default function RootLayout({
   children,
---
+++ app/lib/actions.ts
@@ -0,0 +1,130 @@
+'use server';
+
+import { signIn } from '@/auth';
+import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod'
+
+const FormSchema = z.object({
+    id: z.string({
+      invalid_type_error: 'Please select a customer.'
+    }),
+    customerId: z.string(),
+    amount: z.coerce.number().gt(0,{ message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.'
+    }),
+    date: z.string()
+})
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+
+  console.log(validatedFields);
+  
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Faild to create invoice.'
+    }
+  }
+
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0]
+
+
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+  } catch (error) {
+    return {
+      message: 'Database error: Faild to create invoice.'
+    }
+  }
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return {
+      message: 'Database error: failed to update invoice. '
+    }    
+  }
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id:string) {
+
+  // throw new Error("Failed to Delete Invoice");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return {message: 'Deleted Invoice.'}
+  } catch (error) {
+    return {message: 'Database error: Failed to delete invoice. '}
+  }
+}  
+
+
+export async function authenticate(
+  prevState: string,
+  formData: FormData
+){
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if(error instanceof AuthError){
+      switch (error.type){
+        case 'CredentialsSignin':
+          return 'Invalid credentials. '
+        default:
+          return 'Something went wrong. '
+      }
+    }
+    throw error; 
+  }
+}
